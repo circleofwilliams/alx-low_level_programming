704f383337c24b214e053aef3e269cba3e6f640a
Olamide Williams <circleofwilliams@gmail.com>
2022-06-02 16:24:44 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<unistd.h>
 
 /**
  * main - entry point
@@ -8,9 +9,9 @@
 
 int main(void)
 {
-	char str1[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(str1, sizeof(char),60);
+	write(2, str1, 59);
 
 	return (1);
 }
