6ae2b667b2e649d4ee4f9ad9dd14ac1647189e42
Olamide Williams <circleofwilliams@gmail.com>
2022-06-05 01:43:26 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints alphabets
+ *
+ * Return: Alwasy 0
+ */
+int main()
+{
+	char alp[27] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+		putchar('\n');
+	}
+	return (0);
+}
