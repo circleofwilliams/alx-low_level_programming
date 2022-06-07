babf372fb6b4f0bab719479af6bc9a6a06283ce4
Olamide Williams <circleofwilliams@gmail.com>
2022-06-07 13:06:04 -0700

    Print lower case alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 97;
+
+ 	for (k = 0; k < 10; k++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(i);
+			i++;
+		}
+	_putchar(10);
+	}
+}
+
