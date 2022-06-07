05b8784de5927f05f285c88587907d4bf21d436f
Olamide Williams <circleofwilliams@gmail.com>
2022-06-07 12:19:40 -0700

    Print lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets
+ */
+void print_alphabet(void)
+{
+	int alp[], i;
+
+	for (i = 97; i < 113; i++)
+	{
+		_putchar(alp[i]);
+		_putchar(10);
+	}
+}
+
+/**
+ * main - checks the code.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
