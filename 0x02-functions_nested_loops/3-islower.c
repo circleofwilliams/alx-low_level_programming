51c0d57b7bfaf701f5356b13c6cedcb9c7624811
Olamide Williams <circleofwilliams@gmail.com>
2022-06-07 13:54:53 -0700

    Checking upper or lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ *_islower - checks for lowercase character
+ *Return: 0 if lower or 1 if upper
+ *@c: character in ASCII code
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (0);
+	}
+	_putchar(10);
+}
