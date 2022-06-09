c15840b011b01f57f71bbffdd6a41925668e7fcc
Olamide Williams <circleofwilliams@gmail.com>
2022-06-09 08:42:09 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*more_numbers - prints 10* number
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+	int k;
+
+	i[30] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+	for (j = 0; j < 10; j++)
+	{
+		for (k = 0; k < 15; k++)
+		{
+			_putchar(i[k]);
+		}
+		_putchar(10);
+	}
+	_putchar(10);
+}
