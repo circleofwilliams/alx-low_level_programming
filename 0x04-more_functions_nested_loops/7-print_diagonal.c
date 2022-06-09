b61f77a96919878fccb7afb488a307c1baee626c
Olamide Williams <circleofwilliams@gmail.com>
2022-06-09 14:40:43 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_diagonal - print diagonal lines on the screen
+*@n: number of times the line will be printed
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		if (i < (n - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
