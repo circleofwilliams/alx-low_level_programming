95d08ea87fc0389d21aa1472d40c039883cff1da
Olamide Williams <circleofwilliams@gmail.com>
2022-06-09 07:50:18 -0700

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
-*print_numbers - prints 0-9
+*print_numbers - print numbers 0-9
 */
 
 void print_numbers(void)
 {
-	int j;
+	int i;
 
-	int i[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	i = 48;
 
-	for (j = 0; j < 10; j++)
+	for (i = 48; i <= 57; i++)
 	{
-		_putchar(i[j]);
+		_putchar(i);
 	}
 	_putchar(10);
 }
