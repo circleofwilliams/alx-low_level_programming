60b592e7c6ce185d1ce9cbe1634b46cede7aa797
Olamide Williams <circleofwilliams@gmail.com>
2022-06-09 21:30:37 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+*main - prints digits and fizzbuzz
+*Return: always 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (((i % 3) && (i % 5)) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("%d ", 1);
+}
