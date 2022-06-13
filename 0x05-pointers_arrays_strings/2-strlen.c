dca107a3ad33d5d84914c109166d5cd3bd494b34
Olamide Williams <circleofwilliams@gmail.com>
2022-06-13 10:35:16 -0700

    2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,11 +3,12 @@
 /**
 *_strlen - finds the length of a string
 *@s: variable to find its length
+*Return: return the length of string
 */
 
 int _strlen(char *s)
 {
-	int count = 1;
+	int count = 0;
 
 	while (*s++ != '\0')
 		count++;
