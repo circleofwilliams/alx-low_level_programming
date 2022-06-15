d8875e7971e55eea3bf53582da3f52059fad1093
Olamide Williams <circleofwilliams@gmail.com>
2022-06-15 08:40:42 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+*_strcat - concatonate two strings
+*@dest: string one
+*@src: string two
+*Return: returns the concatonated string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	char *keep = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (keep);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef main_h_
+#define main_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stirng.h>
+
+char *_strcat(char *dest, char *src);
+
+#endif
