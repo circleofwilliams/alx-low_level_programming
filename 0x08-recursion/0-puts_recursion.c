394d8a17dd8b3d67789b338b753ef5e47ded0413
Olamide Williams <circleofwilliams@gmail.com>
2022-06-22 08:46:12 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*_puts_recursion - prints a string
+*@s: string to print
+*/
+
+void _puts_recursion(char *s)
+{
+	return (puts(s));
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char _putchar(char);
+void _puts_recursion(char *s);
+
+#endif
