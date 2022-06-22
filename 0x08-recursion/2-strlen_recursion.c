5af5ce4a6ffaba560fb700dfaab8c21487471fc5
Olamide Williams <circleofwilliams@gmail.com>
2022-06-22 09:56:52 -0700

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "mian.h"
+
+/**
+*_strlen_recursion - measures length of a string
+*@s: the string to mesasure
+*Return: the length
+*/
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+	if (*s != '\0')
+		{
+			++len;
+			_strlen_recursion(s + 1);
+		}
+	return (len);
+}
