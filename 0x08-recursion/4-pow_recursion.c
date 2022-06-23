ce8ddf119a58ca69cdf99e63b8fd319855044623
Olamide Williams <circleofwilliams@gmail.com>
2022-06-23 02:14:06 -0700

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,15 +4,15 @@
 *_pow_recursion - raises x to power y
 *@x: the number
 *@y: the power
-*Return: power or -1 
+*Return: power or -1
 */
 
 int _pow_recursion(int x, int y)
 {
 	if (y == 0)
 		return (1);
- 	else if (y > 0)
+	else if (y > 0)
 		return (x * _pow_recursion(x, y - 1));
-	else 
+	else
 		return (-1);
 }
