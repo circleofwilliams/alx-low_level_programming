1a105339fbdecc8cda56dac17c04ae4ea2bd5c00
Olamide Williams <circleofwilliams@gmail.com>
2022-06-23 02:56:11 -0700

    6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,6 +14,6 @@ int _pow_recursion(int x, int y);
 int power_operation(int n, int c);
 int _sqrt_recursion(int n);
 int is_prime(int n, int i);
-int is _prime_number(int n);
+int is_prime_number(int n);
 
 #endif
