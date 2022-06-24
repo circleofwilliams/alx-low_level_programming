034890f781c9be6f1ac01a89760f28780b9cd554
Olamide Williams <circleofwilliams@gmail.com>
2022-06-24 14:39:04 -0700

    2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,11 +3,14 @@
 /**
 *main - prints all arguments it recieves
 *@argv: array of characters
+*@argc: argument number
+*Return: o is success
 */
 
 int main(int argc, char **argv)
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
