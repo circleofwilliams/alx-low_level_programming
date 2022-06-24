2794f5dcff4ce1e53c2f1addc31de6380cd6aa2e
Olamide Williams <circleofwilliams@gmail.com>
2022-06-24 13:48:52 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints out the programe name
+* @argc: number of command line arguments
+* @argv: string of command line arguments
+* Return: 0 is success
+*/
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+##This directory contains files that test for knowledge on parameter to the main function
