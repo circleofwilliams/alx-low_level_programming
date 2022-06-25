1e2e46e273c6867b76868b294b4e73db2a27ad73
Olamide Williams <circleofwilliams@gmail.com>
2022-06-24 21:27:52 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,13 +9,14 @@
 
 int main(int argc, char **argv)
 {
-	if (argc > 2)
+	if (argc < 3)
 	{
-		printf("%d\n", argv[1] * argv[2]);
+		printf("Error\n");
+		return (1);
 	}
 	else
 	{
-		printf("Error\n");
+		printf("%d \n", argv[1] * argv[2]);
 	}
-	return (1);
+	return (0);
 }
