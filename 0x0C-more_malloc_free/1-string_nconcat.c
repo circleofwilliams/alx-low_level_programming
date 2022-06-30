51046583a0b5afec015042d607ba39014e8772cb
Olamide Williams <circleofwilliams@gmail.com>
2022-06-30 12:52:21 -0700

    A pointer concatonating

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,18 @@
 #include "main.h"
 
+/**                                                       
+*_strlen - find length of string                          
+*@str: the string                                         
+*Return: the lenght                                       
+*/                                                        
+                                            
+int _strlen(char *str)                                    
+{                                                         
+	if (*str = '\0')                                  
+		return (0);                               
+	return (1 + _strlen(str + 1));                    
+}                              
+
 /**
 *string_nconcat - concatonate 2 strings
 *@s1: lead string
