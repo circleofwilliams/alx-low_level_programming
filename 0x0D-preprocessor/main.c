5f7e5bed1693329f9bfdbe63fb090e0303a9d89c
Olamide Williams <circleofwilliams@gmail.com>
2022-07-04 10:08:35 -0700

    print file name

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*main - prints the name of file
+*Return: 0 if success and any number if otherwise
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,9 @@
+#ifndef __main_h__
+#define __main_h__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+char _putchar(char);
+
+#endif
