4cc4a4ecdadff963552427e2ac2e9bd9c8fc65f6
Olamide Williams <circleofwilliams@gmail.com>
2022-07-05 02:29:03 -0700

    Print the model

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,16 +8,18 @@
 
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d == NULL)
+		exit(1);
+	else
 	{
-		if (!(d->name))
+		if (d->name == NULL)
 			printf("Name: (nill)\n");
 		else
 			printf("Name: %s\n", d->name);
 
 		printf("Age: %f\n, d->age");
 
-		if (!(d->owner))
+		if (d->owner == NULL)
 			printf("Owner: (nill)\n");
 		else
 			printf("Owner: %s\n", d->owner);
