c36dd6228c2dcdd437ee226b393c6248aba783ed
Olamide Williams <circleofwilliams@gmail.com>
2022-07-06 08:52:23 -0700

    Print a name

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+# This foler contains files that tests for pointers to functions
+
+	>function pointers ((*fun_ptr)();)
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef _function_pointers_
+#define _function_pointers_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char _putchar(char);
+void print_name(char *name, void (*f)(char *));
+
+#endif
