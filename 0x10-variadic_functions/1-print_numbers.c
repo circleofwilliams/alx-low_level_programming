25bf4cfdc497e8d2c2e775ecfd338f7681ccdf02
Olamide Williams <circleofwilliams@gmail.com>
2022-07-08 06:54:10 -0700

    Print unknown number of numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,15 +2,15 @@
 
 /**
 *print_numbers - prints numbers
-@s: the string to be between
-@n: numbers of integers passed to the function
+*@s: the string to be between
+*@n: numbers of integers passed to the function
 */
 
 void print_numbers(const char *s, const size_t n, ...)
 {
 	size_t i;
-
 	va_list arg;
+
 	va_start(arg, n);
 
 	for (i = 0; i < n; i++)
