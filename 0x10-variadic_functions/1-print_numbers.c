e8411046c4261a31c4f87a9040aeec23750e56a1
Olamide Williams <circleofwilliams@gmail.com>
2022-07-08 06:48:12 -0700

    Print unknown number of numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+*print_numbers - prints numbers
+@s: the string to be between
+@n: numbers of integers passed to the function
+*/
+
+void print_numbers(const char *s, const size_t n, ...)
+{
+	size_t i;
+
+	va_list arg;
+	va_start(arg, n);
+
+	for (i = 0, i < n; i++)
+	{
+		printf("%d", va_arg(arg, int));
+		if (s == NULL)
+			continue;
+		else if (i < n - 1)
+			printf("%s", s);
+	}
+	printf("\n");
+	va_end(arg);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,6 @@
 #include <stdarg.h>
 
 int sum_them_all(const size_t n, ...);
+void print_numbers(const char*, const size_t, ...);
 
 #endif
