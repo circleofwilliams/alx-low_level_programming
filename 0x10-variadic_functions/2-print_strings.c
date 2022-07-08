45431647b715a6f3cb0773ec7c294c05ed7fe103
Olamide Williams <circleofwilliams@gmail.com>
2022-07-08 07:24:58 -0700

    Print all strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+*print_strings - prints strings
+*@s: seperator
+*@n: number of strings
+*/
+
+void print_strings(const char *s, const size_t n, ...)
+{
+	size_t i;
+	char *str;
+	va_list arg;
+
+	va_start(arg, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(arg, char *);
+		if (str == NULL)
+			printf("(nill)");
+		printf("%s", str);
+		if (s == NULL)
+			continue;
+		else if (i < n - 1)
+			printf("%s", s);
+	}
+
+	printf("\n");
+	va_end(arg);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,6 @@
 
 int sum_them_all(const size_t n, ...);
 void print_numbers(const char*, const size_t, ...);
+void print_strings(const char*, const size_t, ...);
 
 #endif
